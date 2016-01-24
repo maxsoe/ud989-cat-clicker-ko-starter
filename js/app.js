3bf93f13e76ee607ae8fb7d43df28f5d2e244b52
Max Soe <maxsoe@gmail.com>
2016-01-24 12:14:32 +0000

    Cat Clicker HTML and Bindings

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var viewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribution = ko.observable('https://www.flickr.com/photos/whoever');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+}
+
+ko.applyBindings(new viewModel());
